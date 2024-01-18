167984d5a41e379d0ec3d54d5664060ece29ae5a
Jace-Banford-Byington <100317211+Jace-Banford-Byington@users.noreply.github.com>
2024-01-18 10:53:50 -0700

    Started working on the Navbar

--- front-end/src/Components/Navbar.js
+++ front-end/src/Components/Navbar.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 
-const Navbar = () => {
+const NavBar = () => {
   return (
-    <div>Navbar</div>
+   <Navbar bg= 'dark' expand="lg">
+    <Container>
+    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+    </Container>
+    </Navbar>
   )
 }
 
-export default Navbar
+export default NavBar
 
 //The bar that goes across the screen 
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
+
+import Navbar from './Components/Navbar'; 
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
   </React.StrictMode>
 );
 
