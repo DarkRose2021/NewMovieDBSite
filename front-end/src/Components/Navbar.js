d733b879cb4d6d8e656ed72649ffea190fe4ca71
Jace-Banford-Byington <100317211+Jace-Banford-Byington@users.noreply.github.com>
2024-01-18 18:11:17 -0700

    Navbar has things on it.
    The nav bar is the basic what can be found on bootstrap

--- front-end/src/Components/Navbar.js
+++ front-end/src/Components/Navbar.js
@@ -4,15 +4,23 @@ import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 const NavBar = () => {
   return (
    <Navbar bg= 'dark' expand="lg">
-    <Container>
-    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+     <Container>
+        <Navbar.Brand href="#home">[Name of Website]</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
-    </Container>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   )
 }
