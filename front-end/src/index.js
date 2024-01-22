b40ca6fe7a86f61653420c4dc5c6287c4c1c2f44
Jace-Banford-Byington <100317211+Jace-Banford-Byington@users.noreply.github.com>
2024-01-22 16:55:54 -0700

    Index.js routing set up

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,15 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
+import NavBar from './Components/Navbar';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import SignUp from './Components/SignUp';
+
+const url = "http://localhost:8080"
+
+const router = createBrowserRouter([
+  {
+    //Home page 
+    path:'/',
+    element: <>
+      <NavBar />
+      <Home />
+    </>
+  },
+  {
+    //Login 
+    path:"/login",
+    element: <>
+      <NavBar />
+      <Login />
+    </>
+
+  },
+  {
+    //Signup
+    path: "/signup",
+    element: <>
+      <NavBar />
+      <SignUp />
+    </>
+  },
+
+])
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router}/>  
+</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
