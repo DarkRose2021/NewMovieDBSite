548ba2c0acc8c32fe602c208388265108f6b2bec
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2024-02-06 19:43:15 -0700

    Fixed the signup form

--- front-end/src/Components/SignUp.js
+++ front-end/src/Components/SignUp.js
@@ -1,99 +1,101 @@
 //form to sign up for an account
-import React, {useState} from "react";
-import {useForm} from 'react-hook-form'
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 const SignUp = () => {
-    //api call: `http://localhost:8080/signup`
+	//api call: `http://localhost:8080/signup`
 
-    const {
-        register, 
-        handleSubmit, 
-        formState: {errors},
-        watch} = useForm();
-        
-        const [showPassword, setShowPassword] = useState(false);
-        
-        const password = watch(["password", ""]);
-        
-        const togglePassword = () => {
-            setShowPassword(!showPassword);
-          };
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		watch,
+	} = useForm();
 
+	const [showPassword, setShowPassword] = useState(false);
 
-        const onSubmit = async(data) => {
-            console.log("Data sending: ", data)
-            try{
-                const response = await fetch("http://localhost:8080/signup",{
-                    method: 'POST',
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify(data),
-                });
-                if(!response.ok){
-                    throw new Error(`HTTP Error! Status: ${response.status}`)
-                }
-                const result = await response.json();
-                console.log("Backend response:", result);
-            
-            } 
-            catch (error) {
-                console.error("Error sending data to the backend:", error);
-            }
-        }
+	const togglePassword = () => {
+		setShowPassword(!showPassword);
+	};
 
-    //const username = req.body.username;
-		// const password = req.body.password;
-		// const name = req.body.name;
+	const passwordPattern =
+		/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{10,}$/;
+
+	const onSubmit = async (data) => {
+		console.log("Data sending: ", data);
+		try {
+			const response = await fetch("http://localhost:8080/signup", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(data),
+			});
+			if (!response.ok) {
+				throw new Error(`HTTP Error! Status: ${response.status}`);
+			}
+			const result = await response.json();
+			console.log("Backend response:", result);
+		} catch (error) {
+			console.error("Error sending data to the backend:", error);
+		}
+	};
+
 	return (
-    <div className="signUp" >
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("userName", {required: "Username is required"})} placeholder="Username"/>
+		<div className="signUp">
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<input
+					{...register("username", { required: "Username is required" })}
+					id="username"
+					placeholder="Username"
+				/>
+				<input
+					{...register("name", { required: "Name is required" })}
+					id="name"
+					placeholder="name"
+				/>
 
-        <input
-          {...register("password", {
-            required: "Password is required",
-            pattern: {
-              value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{10,}$/,
-              message: "Password must include at least one letter, one number, and one special character",
-            },
-            minLength: {
-                value: 10,
-                message: "Password must be at least 10 characters",
-              },
-          })}
-          type={showPassword ? "text" : "password"}
-          placeholder="Password" />
+				<input
+					id="password"
+					{...register("password", {
+						required: "Password is required",
+						pattern: {
+							value: passwordPattern,
+							message:
+								"Password must include at least one letter, one number, and one special character",
+						},
+						minLength: {
+							value: 10,
+							message: "Password must be at least 10 characters",
+						},
+					})}
+					type={showPassword ? "text" : "password"}
+					placeholder="Password"
+				/>
 
-          {errors.password && <p>{errors.password.message}</p>}
-        
-          <input
-          {...register("passwordConfirm", {
-            required: "Please confirm your password",
-            validate: (value) => value === password || "Passwords do not match",
-          })}
-          type={showPassword ? "text" : "password"}
-          placeholder="Confirm Password"
-        />
-            <span className="icon-eye" onClick={togglePassword}>
-                                {showPassword ? (
-                                    <i className="bi bi-eye"></i>
-                                ) : (
-                                    <i className="bi bi-eye-slash"></i>
-                                )}
-                                </span>
-        {errors.passwordConfirm && <p>{errors.passwordConfirm.message}</p>}
+				{errors.password && <p>{errors.password.message}</p>}
 
+				<input
+					{...register("passwordConfirm", {
+						required: "Please confirm your password",
+						validate: (value) =>
+							value === watch("password", "") || "Passwords do not match",
+					})}
+					type={showPassword ? "text" : "password"}
+					placeholder="Confirm Password"
+				/>
+				<span className="icon-eye" onClick={togglePassword}>
+					{showPassword ? (
+						<i className="bi bi-eye"></i>
+					) : (
+						<i className="bi bi-eye-slash"></i>
+					)}
+				</span>
+				{errors.passwordConfirm && <p>{errors.passwordConfirm.message}</p>}
 
-            <input type="submit" />
-        </form>
-
-
-
-
-
-
-    </div>  
-    );
+				<input type="submit" />
+			</form>
+		</div>
+	);
 };
 
 export default SignUp;
