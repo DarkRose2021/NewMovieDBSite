87a66b7e08cebf1b6abca843858895e1996b9ab6
Jace-Banford-Byington <100317211+Jace-Banford-Byington@users.noreply.github.com>
2024-03-06 22:02:38 -0700

    Movie and Actor Search works

--- front-end/src/Components/Search.js
+++ front-end/src/Components/Search.js
@@ -1,86 +1,118 @@
-//search bar  and its functionality with the back end
-
-// 				BAck end 
-// app.get("/searchByGenre/:genre", async (req, res) => {
-// 	let genre = req.params.genre;
-// 	let encodedString = encodeURIComponent(genre);
-// 	const genreJson = await fetchGenres();
-// 	genreMapping = genreJson.genres.reduce((acc, genre) => {
-// 		acc[genre.id] = genre.name;
-// 		return acc;
-// 	});
-// 	let searchedGenre = await searchGenres(encodedString);
-// 	res.json(searchedGenre);
-// });
-
-// // Finished
-// app.get("/searchByActor/:actor", async (req, res) => {
-// 	let actor = req.params.actor;
-// 	let encodedString = encodeURIComponent(actor);
-// 	const genreJson = await fetchGenres();
-// 	genreMapping = genreJson.genres.reduce((acc, genre) => {
-// 		acc[genre.id] = genre.name;
-// 		return acc;
-// 	});
-// 	let searchedPerson = await searchActors(encodedString);
-// 	const actorsWithDetails = await updateActorMovieDetails(
-// 		searchedPerson,
-// 		genreMapping
-// 	);
-// 	res.json(actorsWithDetails);
-// });
-
-
-
 import React, { useState } from "react";
+import { Dropdown } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [movieList, setMovieList] = useState([]);
-  const [actorList, setActorList] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchCategory, setSearchCategory] = useState('Movie'); 
 
   const handleInputChange = async (event) => {
     setSearchTerm(event.target.value);
     try {
-      const response = await fetch(`http://localhost:8080/searchByTitle/${event.target.value}`);
+      let endpoint;
+      switch (searchCategory) {
+        case 'Movie':
+          endpoint = 'searchByTitle';
+          break;
+        case 'Actor':
+          endpoint = 'searchByActor';
+          break;
+        case 'Genre':
+          endpoint = 'searchByGenre';
+          break;
+        default:
+          endpoint = 'searchByTitle';
+      }
+      const response = await fetch(`http://localhost:8080/${endpoint}/${event.target.value}`);
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
       const data = await response.json();
       console.log("Search results:", data);
-      if (data && data.length > 0) {
-        setMovieList(data);
-      } else {
-        setMovieList([]);
-      }
+      setSearchResults(data);
     } catch (error) {
       console.error("Error searching:", error);
     }
   }
-  
+
+  const handleCategoryChange = (category) => {
+    setSearchCategory(category);
+    setSearchResults([]); // Clear search results when changing category
+  };
+
+  const renderMovieResults = () => (
+    <div className="movieResults">
+      <ul>
+        {searchResults.map(movie => (
+          <li key={movie.id}>
+            <Link to={`/movie/${movie.id}`}>
+              <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title} />
+              <br />
+              <h1 className="movieTitle">{movie.title}</h1>	
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+
+  const renderActorResults = () => (
+    <div className="actorResults">
+      <ul>
+        {searchResults.map(actor => (
+          <li key={actor.id}>
+            <Link to={`/actor/${actor.id}`}>
+              <img src={`https://image.tmdb.org/t/p/original/${actor.profile_path}`} alt={actor.name} />
+              <br />
+              <h1 className="actorName">{actor.name}</h1>	
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+
+  const renderGenreResults = () => (
+    <div className="genreResults">
+      <ul>
+        {searchResults.map(movie => (
+          <li key={movie.id}>
+            <Link to={`/movie/${movie.id}`}>
+              <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title} />
+              <br />
+              <h1 className="movieTitle">{movie.title}</h1>	
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+
   return (
     <div>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          {searchCategory}
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item onClick={() => handleCategoryChange('Movie')}>Movie</Dropdown.Item>
+          <Dropdown.Item onClick={() => handleCategoryChange('Actor')}>Actor</Dropdown.Item>
+          <Dropdown.Item onClick={() => handleCategoryChange('Genre')}>Genre</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
       <input 
         className="searchBar"
         type="search"
-        placeholder="Search"
+        placeholder={`${searchCategory} Search`} 
         value={searchTerm}
         onChange={handleInputChange}
       />
       
-      <div className="movieCards">
-        {movieList.length > 0 && (
-          <ul>
-            {movieList.map(movie => (
-              <li key={movie.id}>
-				<img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title} />
-				<br />
-				{movie.title}
-			  </li>
-            ))}
-          </ul>
-        )}
-      </div>
+      {searchCategory === 'Movie' && renderMovieResults()}
+      {searchCategory === 'Actor' && renderActorResults()}
+      {searchCategory === 'Genre' && renderGenreResults()}
     </div>
   );
 };
